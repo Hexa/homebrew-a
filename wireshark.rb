d5468c10334b8637235a4ddfb8243dda40ff3679
Hexa <hexa.diary@gmail.com>
2012-07-08 02:13:19 +0900

    fix hash

--- wireshark.rb
+++ wireshark.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Wireshark < Formula
   homepage 'http://www.wireshark.org'
   url 'http://www.wireshark.org/download/src/all-versions/wireshark-1.8.0.tar.bz2'
-  sha1 '3eca81253800a0089d0f957e75853b05'
+  sha1 'd45b2828fc6dfac697f752f146e6cf43f4776047'
 
   def install
     system "./configure", "--prefix=#{prefix}", "--with-ssl"
