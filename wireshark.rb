19118545f0fed1dc07c4e55454da5a7cf2b6060d
Hexa <hexa.diary@gmail.com>
2012-09-09 17:05:16 +0900

    Update Wireshark to 1.8.2

--- wireshark.rb
+++ wireshark.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Wireshark < Formula
   homepage 'http://www.wireshark.org'
-  url 'http://www.wireshark.org/download/src/all-versions/wireshark-1.8.0.tar.bz2'
-  sha1 'd45b2828fc6dfac697f752f146e6cf43f4776047'
+  url 'http://www.wireshark.org/download/src/all-versions/wireshark-1.8.2.tar.bz2'
+  sha1 '4737d9745dbf002444ea42615243abf3bb80b943'
 
   def install
     system "./configure", "--prefix=#{prefix}", "--with-ssl"
